878151032012a2f977037847bbb361980a1f6717
Hacker-KM <kartik.mishra3603@gmail.com>
2024-02-10 16:04:12 +0530

    Fetching Done

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,9 +3,22 @@ import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
 const Dashboard = () => {
+  const {isLoading} = React.useContext(GithubContext)
+  if(isLoading){
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img src={loadingImage} className='loading-img' alt="loading"/>
+        <Info />
+        <User />
+        <Repos />
+      </main>
+    );
+  }
   return (
     <main>
-      {/* <Navbar/> */}
+      <Navbar/>
       <Search/>
       <Info/>
       <User/>
