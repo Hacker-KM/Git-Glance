a55e26092b737543239e10dab493628073677003
Hacker-KM <kartik.mishra3603@gmail.com>
2024-02-17 15:29:38 +0530

    Project Completed

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-kme6vjdohsuqjjx4.us.auth0.com"
+      clientId="SXjmktevLusbdaDNe6oWh4rd9ihtuL0P"
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
